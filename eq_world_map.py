b6e9c886ab95e140d981bd0a3f99fee355d745b6
marius <mariuss_boy98@yahoo.com>
2024-02-04 18:33:34 +0100

    Representing Magnitudes over a span of 30 days.

--- eq_world_map.py
+++ eq_world_map.py
@@ -4,7 +4,7 @@ import json
 import plotly.express as px
 
 # Read data as a string and convert to a Python object.
-path = Path('/Users/mariusoprea/Desktop/python_work/Part II-Projects/Chapter_16_Downloading_Data/Downloading_Data/eq_data/eq_1_day_m1.geojson')
+path = Path('/Users/mariusoprea/Desktop/python_work/Part II-Projects/Chapter_16_Downloading_Data/Downloading_Data/eq_data/eq_data_30_day_m1.geojson')
 contents = path.read_text()
 all_eq_data = json.loads(contents)
 
@@ -23,5 +23,5 @@ for eq_dict in all_eq_dicts:
     lats.append(lat)
 
 title = 'Global Earthquakes'
-fig = px.scatter_geo(lat=lats, lon=lons, title=title)
+fig = px.scatter_geo(lat=lats, lon=lons, size=mags, title=title)
 fig.show()
